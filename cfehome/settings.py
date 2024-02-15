7d00ea24b2151d9da4b68c77dc5104341fbf24ea
fadingreality1 <kunalverma.learn@gmail.com>
2024-02-16 04:41:46 +0530

    Default Rest_framework setting

--- cfehome/settings.py
+++ cfehome/settings.py
@@ -124,3 +124,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        "api.authentication.TokenAuthenticationAugmented",
+    ],
+    'DEFAULT_PERMISSION_CLASSES':[
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+    ],
+}
